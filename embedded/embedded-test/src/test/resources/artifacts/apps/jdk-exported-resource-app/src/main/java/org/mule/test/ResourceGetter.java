d9033cfb4215de148915c2db952e8df7e35238e4
Juan Desimoni <juan.desimoni@mulesoft.com>
2018-06-13 17:51:19 -0300

    MULE-15028: Embedded container does not expose JDK resources (#154)

---
+++ embedded/embedded-test/src/test/resources/artifacts/apps/jdk-exported-resource-app/src/main/java/org/mule/test/ResourceGetter.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.test;
+
+import java.io.InputStream;
+
+public class ResourceGetter {
+
+  public static InputStream getJdkResource() {
+    return Thread.currentThread().getContextClassLoader().getResourceAsStream("javax/swing/text/rtf/charsets/ansi.txt");
+  }
+}
