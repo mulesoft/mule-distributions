7c2216043f19191cc1a61304b2eae349a50fe739
pablolagreca <Neruda10>
2018-09-13 17:48:35 -0300

    MULE-15480 - Race condition in Embedded tests

--- embedded/embedded-test/src/test/java/org/mule/runtime/module/embedded/ApplicationTestCase.java
+++ embedded/embedded-test/src/test/java/org/mule/runtime/module/embedded/ApplicationTestCase.java
@@ -51,7 +51,6 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -76,46 +75,43 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
 
   @Description("Embedded runs an application depending on a connector")
   @Test
-  @Ignore("MULE-15480")
   public void applicationWithConnector() throws Exception {
     BundleDescriptor bundleDescriptor = getApplicationBundleDescriptor("http-echo", empty());
-    doWithinApplication(bundleDescriptor, getAppFolder("http-echo"), port -> {
+    doWithinApplication(bundleDescriptor, getAppFolder("http-echo"), createRetryTestOperation(port -> {
       assertTestMessage(port);
-    });
+    }));
   }
 
   @Description("Embedded runs an application that retrieves a resource from the JDK")
   @Test
-  @Ignore("MULE-15480")
   // This test may fail depending on the JDK used to run the tests
   public void jdkResourceAvailableFromApp() throws Exception {
     BundleDescriptor bundleDescriptor = getApplicationBundleDescriptor("jdk-exported-resource-app", empty());
-    doWithinApplication(bundleDescriptor, getAppFolder("jdk-exported-resource-app"), port -> {
+    doWithinApplication(bundleDescriptor, getAppFolder("jdk-exported-resource-app"), createRetryTestOperation(port -> {
       try {
         HttpResponse<String> response = post(format("http://localhost:%s/", port)).asString();
         assertThat(response.getBody(), containsString("# The character set used to read documents with the \\ansi control"));
       } catch (UnirestException e) {
         throw new RuntimeException(e);
       }
-    });
+    }));
   }
 
   @Description("Embedded runs an application declaring a remote repository for a dependency")
   @Test
-  @Ignore("MULE-15480")
   public void applicationWithRemoteRepositories() throws Exception {
     String appName = "pom-with-remote-repositories";
     BundleDescriptor bundleDescriptor = getApplicationBundleDescriptor(appName, empty());
-    doWithinApplicationNotInstalled(bundleDescriptor, getAppFolder(appName), ApplicationTestCase::assertTestMessage);
+    doWithinApplicationNotInstalled(bundleDescriptor, getAppFolder(appName),
+                                    createRetryTestOperation(ApplicationTestCase::assertTestMessage));
   }
 
   @Description("Embedded runs an application using test dependencies and deploying a jar file")
   @Test
-  @Ignore("MULE-15480")
   public void applicationWithTestDependency() throws Exception {
     BundleDescriptor bundleDescriptor =
         getApplicationBundleDescriptor("http-test-dependency", of("mule-application-light-package"));
-    doWithinApplication(bundleDescriptor, getAppFolder("http-test-dependency-light-package"), port -> {
+    doWithinApplication(bundleDescriptor, getAppFolder("http-test-dependency-light-package"), createRetryTestOperation(port -> {
       try {
         String httpBody = "Something";
         HttpResponse<String> response = post(format("http://localhost:%s/", port)).body(httpBody).asString();
@@ -123,7 +119,7 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
       } catch (UnirestException e) {
         throw new RuntimeException(e);
       }
-    });
+    }));
   }
 
   @Description("Embedded runs an application in lazy init mode")
@@ -171,12 +167,11 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
 
   @Description("Embedded runs an application using a custom log4j configuration file")
   @Test
-  @Ignore("MULE-15480")
   public void applicationWithCustomLogger() throws Exception {
     BundleDescriptor bundleDescriptor = getApplicationBundleDescriptor("http-echo", empty());
-    doWithinApplication(bundleDescriptor, getAppFolder("http-echo"), port -> {
+    doWithinApplication(bundleDescriptor, getAppFolder("http-echo"), createRetryTestOperation(port -> {
       assertTestMessage(port);
-    }, false, true, true,
+    }), false, true, true,
                         of(getClass().getClassLoader().getResource("log4j2-custom-file.xml").toURI()), true);
     try {
       File expectedLoggingFile = new File(LOGGING_FILE);
@@ -189,14 +184,13 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
 
   @Test
   @Description("Deploys an app with an http listener an checks that communication works")
-  @Ignore("MULE-15481")
   public void deployListenerIsAlive() throws Exception {
     BundleDescriptor bundleDescriptor = getApplicationBundleDescriptor("test-app", empty());
     File artifactFile = installMavenArtifact(getAppFolder("successful-app"), bundleDescriptor);
     runWithContainer((container) -> {
       container.getDeploymentService()
           .deployApplication(ArtifactConfiguration.builder().artifactLocation(artifactFile).build());
-      assertAppIsRunning(true);
+      executeWithRetry(() -> assertAppIsRunning(true));;
     });
   }
 
@@ -226,7 +220,6 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
 
   @Test
   @Description("If a well written app with the same name as a failing app is deployed after the failing one, it should work")
-  @Ignore("MULE-15481")
   public void redeploymentOfSuccessfulAppAfterFailingWithSameNameShouldWork() throws Exception {
     runWithContainer((container) -> {
       BundleDescriptor bundleDescriptor = getApplicationBundleDescriptor("test-app", empty());
@@ -239,7 +232,7 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
       // failing app.
       container.getDeploymentService()
           .deployApplication(ArtifactConfiguration.builder().artifactLocation(testAppLocation).build());
-      assertAppIsRunning(true);
+      executeWithRetry(() -> assertAppIsRunning(true));
     });
   }
 
@@ -296,7 +289,6 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
 
   @Test
   @Description("Even if 2 apps have the same name and were created at the same time, if one of them have different config files, redeployment should be triggered")
-  @Ignore("MULE-15481")
   public void redeploymentOfSuccessfulAppAfterFailingWithSameNameAndTimeStampButDifferentConfigShouldWork() throws Exception {
     runWithContainer((container) -> {
       long time = System.currentTimeMillis();
@@ -310,7 +302,7 @@ public class ApplicationTestCase extends AbstractEmbeddedTestCase {
       container.getDeploymentService()
           .deployApplication(ArtifactConfiguration.builder().artifactLocation(testAppLocation).build());
 
-      assertAppIsRunning(true);
+      executeWithRetry(() -> assertAppIsRunning(true));
     });
   }
 
