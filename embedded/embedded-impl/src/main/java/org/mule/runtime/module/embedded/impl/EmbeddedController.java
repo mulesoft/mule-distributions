82576e3d125a19efa64b07f133918da688389bb2
elrodro83 <rodrigo.merino@mulesoft.com>
2022-06-27 11:20:38 -0300

    set the deployment property to make the artifactAst available

--- embedded/embedded-impl/src/main/java/org/mule/runtime/module/embedded/impl/EmbeddedController.java
+++ embedded/embedded-impl/src/main/java/org/mule/runtime/module/embedded/impl/EmbeddedController.java
@@ -6,16 +6,13 @@
  */
 package org.mule.runtime.module.embedded.impl;
 
-import static java.lang.String.valueOf;
-import static java.lang.System.setProperty;
-import static org.apache.commons.io.FileUtils.toFile;
-import static org.apache.commons.io.FilenameUtils.getName;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getAppsFolder;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getConfFolder;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getDomainFolder;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getDomainsFolder;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getServerPluginsFolder;
 import static org.mule.runtime.container.api.MuleFoldersUtil.getServicesFolder;
+import static org.mule.runtime.core.api.config.MuleDeploymentProperties.MULE_ADD_ARTIFACT_AST_TO_REGISTRY_DEPLOYMENT_PROPERTY;
 import static org.mule.runtime.core.api.config.MuleDeploymentProperties.MULE_LAZY_CONNECTIONS_DEPLOYMENT_PROPERTY;
 import static org.mule.runtime.core.api.config.MuleDeploymentProperties.MULE_LAZY_INIT_DEPLOYMENT_PROPERTY;
 import static org.mule.runtime.core.api.config.MuleDeploymentProperties.MULE_LAZY_INIT_ENABLE_XML_VALIDATIONS_DEPLOYMENT_PROPERTY;
@@ -23,6 +20,12 @@ import static org.mule.runtime.core.api.config.MuleProperties.MULE_HOME_DIRECTOR
 import static org.mule.runtime.core.api.util.FileUtils.unzip;
 import static org.mule.runtime.module.embedded.impl.PathUtils.getPath;
 import static org.mule.runtime.module.embedded.impl.SerializationUtils.deserialize;
+
+import static java.lang.String.valueOf;
+import static java.lang.System.setProperty;
+
+import static org.apache.commons.io.FileUtils.toFile;
+import static org.apache.commons.io.FilenameUtils.getName;
 
 import org.mule.runtime.api.artifact.Registry;
 import org.mule.runtime.api.connectivity.ConnectivityTestingService;
@@ -114,6 +117,8 @@ public class EmbeddedController {
                                valueOf(artifactConfiguration.getDeploymentConfiguration().xmlValidationsEnabled()));
       deploymentProperties.put(MULE_LAZY_CONNECTIONS_DEPLOYMENT_PROPERTY,
                                valueOf(artifactConfiguration.getDeploymentConfiguration().lazyConnectionsEnabled()));
+      deploymentProperties.put(MULE_ADD_ARTIFACT_AST_TO_REGISTRY_DEPLOYMENT_PROPERTY,
+                               valueOf(artifactConfiguration.getDeploymentConfiguration().addArtifactAstToRegistry()));
 
       deploymentTask.deploy(deploymentProperties);
     } catch (IOException e) {
