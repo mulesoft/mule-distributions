e3f99c26334e89be1faaf383eb9335040f6fd940
rbourbonnavarro <rodrigobourbon44@gmail.com>
2023-06-28 22:34:10 -0300

    W-13137702: `slf4j` classes must be loaded from the same class loader from where its implementation is

--- embedded/embedded-test/src/main/java/module-info.java
+++ embedded/embedded-test/src/main/java/module-info.java
@@ -21,6 +21,11 @@ module org.mule.distribution.embedded.test {
   requires org.slf4j;
   requires zip4j;
 
+  // TODO W-13205329 - remove, this is only needed because the requirement of org.slf4j in the boot layer messes with
+  //  the log4j implementation in the class loader created by mule-embedded-api, and the fix is to provide it also at
+  //  the boot layer level
+  requires org.apache.logging.log4j;
+
   exports org.mule.runtime.module.embedded to
       junit;
 
