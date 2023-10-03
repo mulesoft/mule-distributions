cac9e797e852f2485d2f87a19872de41b0ad447f
rbourbonnavarro <rodrigobourbon44@gmail.com>
2023-10-03 12:14:51 -0300

    W-14227143: Fix backwards compatibility with `4.5`

--- embedded/embedded-impl/src/main/java/org/mule/runtime/module/embedded/impl/legacy/DefaultEmbeddedController.java
+++ embedded/embedded-impl/src/main/java/org/mule/runtime/module/embedded/impl/legacy/DefaultEmbeddedController.java
@@ -4,17 +4,21 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.runtime.module.embedded.impl;
+package org.mule.runtime.module.embedded.impl.legacy;
 
 import org.mule.runtime.module.embedded.api.ContainerInfo;
-import org.mule.runtime.module.embedded.api.controller.EmbeddedController;
+import org.mule.runtime.module.embedded.impl.CommonsEmbeddedController;
+import org.mule.runtime.module.embedded.internal.controller.EmbeddedController;
 
 /**
  * Controller class for the runtime. It spins up a new container instance using a temporary folder and dynamically loading the
  * container libraries.
  *
- * @since 4.6
+ * @since 4.5
+ * @deprecated since 4.6 running with Java 17 the implementation used is
+ *             {@link org.mule.runtime.module.embedded.impl.DefaultEmbeddedController}.
  */
+@Deprecated
 public class DefaultEmbeddedController extends CommonsEmbeddedController implements EmbeddedController {
 
   public DefaultEmbeddedController(ContainerInfo containerInfo) {
