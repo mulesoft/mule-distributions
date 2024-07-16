bba81b85d6b6ce360e64647af698c43702ceac6d
rbourbonnavarro <rodrigobourbon44@gmail.com>
2024-07-16 17:02:34 -0300

    W-15884079: Improve encapsulation of the Embedded Runtime (#1333)
    (cherry picked from commit df0f5cbcaeeb10ce0189034391dd156eca5f8491)

--- embedded/embedded-test/tests/src/test/java/org/mule/runtime/module/embedded/EmbeddedLifecycleTestCase.java
+++ embedded/embedded-test/tests/src/test/java/org/mule/runtime/module/embedded/EmbeddedLifecycleTestCase.java
@@ -18,6 +18,7 @@ import static org.mule.test.allure.AllureConstants.EmbeddedApiFeature.EmbeddedAp
 
 import static java.lang.System.getProperty;
 
+import static com.google.common.primitives.Booleans.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
@@ -34,6 +35,7 @@ import org.mule.tck.junit4.rule.SystemProperty;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.Properties;
 
 import org.junit.BeforeClass;
@@ -46,9 +48,14 @@ import io.qameta.allure.Features;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Stories;
 import io.qameta.allure.Story;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 @Features(@Feature(EMBEDDED_API))
 @Stories({@Story(CONFIGURATION), @Story(EMBEDDED)})
+@RunWith(Parameterized.class)
 public class EmbeddedLifecycleTestCase {
 
   private static EmbeddedTestHelper embeddedTestHelper;
@@ -64,6 +71,20 @@ public class EmbeddedLifecycleTestCase {
   @Rule
   public SystemProperty skipModuleTweakingValidation = new SystemProperty("mule.module.tweaking.validation.skip", "true");
 
+  // mule-embedded-api no longer supports log4j configuration since 1.7, this property has to be set so the log4j configuration
+  // doesn't take place
+  @Rule
+  public SystemProperty simpleLogging =
+      new SystemProperty("mule.simpleLog", "true");
+
+  @Parameter
+  public boolean useIsolation;
+
+  @Parameters(name = "useIsolation: {0}")
+  public static Collection<Boolean> data() {
+    return asList(false, true);
+  }
+
   @Test
   public void shouldFailToCreateDueToMissingVersionOfEmbedded() throws IOException, URISyntaxException {
     try {
@@ -76,6 +97,7 @@ public class EmbeddedLifecycleTestCase {
               .build())
           .log4jConfigurationFile(getClass().getClassLoader().getResource("log4j2-default.xml").toURI())
           .product(MULE)
+          .useIsolation(useIsolation)
           .build();
       fail("Should fail to create");
     } catch (IllegalStateException e) {
@@ -101,6 +123,7 @@ public class EmbeddedLifecycleTestCase {
             .build())
         .log4jConfigurationFile(getClass().getClassLoader().getResource("log4j2-default.xml").toURI())
         .product(MULE)
+        .useIsolation(useIsolation)
         .build();
 
     embeddedContainer.start();
@@ -162,7 +185,8 @@ public class EmbeddedLifecycleTestCase {
             .localMavenRepositoryLocation(getLocalRepositoryFolder())
             .build())
         .log4jConfigurationFile(getClass().getClassLoader().getResource("log4j2-default.xml").toURI())
-        .product(MULE);
+        .product(MULE)
+        .useIsolation(useIsolation);
   }
 
 }
