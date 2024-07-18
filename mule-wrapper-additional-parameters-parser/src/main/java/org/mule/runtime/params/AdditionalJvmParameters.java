d4274cec2e396714364af42dc50fafd657117483
rbourbonnavarro <rodrigobourbon44@gmail.com>
2024-07-18 20:15:48 -0300

    Unused import

--- mule-wrapper-additional-parameters-parser/src/main/java/org/mule/runtime/params/AdditionalJvmParameters.java
+++ mule-wrapper-additional-parameters-parser/src/main/java/org/mule/runtime/params/AdditionalJvmParameters.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
